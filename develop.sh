73e881078836abb95da23ff01c5dede6f10d1d88
Eduardo Cifuentes <edcilop@gmail.com>
2019-06-05 00:06:17 -0500

    docker one repository

---
+++ develop.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+
+COMPOSE="docker-compose"
+PS_RESULT=$(docker-compose ps -q)
+
+
+if [[ ! -z "$PS_RESULT" ]]; then
+    EXEC="yes"
+else
+    EXEC="no"
+fi
+
+
+if [[ $# -gt 0 ]]; then
+    if [[ -f .env ]]; then
+        source .env
+    fi
+
+
+    if [[ "$1" == "start" ]]; then
+        ${COMPOSE} up -d
+
+    elif [[ "$1" == "stop" ]]; then
+        ${COMPOSE} down
+
+
+    elif [[ "$1" == "users-art" ]]; then
+        shift 1
+
+        if [[ "$EXEC" == "yes" ]]; then
+            ${COMPOSE} exec users_php \
+                php artisan "$@"
+        else
+            ${COMPOSE} run --rm -w /var/www/html users_php \
+                php artisan "$@"
+        fi
+
+    elif [[ "$1" == "users-composer" ]]; then
+        shift 1
+
+        if [[ "$EXEC" == "yes" ]]; then
+            ${COMPOSE} exec users_php \
+                composer "$@"
+
+        else
+            ${COMPOSE} run --rm -w /var/www/html users_php \
+                composer "$@"
+        fi
+
+
+    elif [[ "$1" == "acl-art" ]]; then
+        shift 1
+
+        if [[ "$EXEC" == "yes" ]]; then
+            ${COMPOSE} exec acl_php \
+                php artisan "$@"
+        else
+            ${COMPOSE} run --rm -w /var/www/html acl_php \
+                php artisan "$@"
+        fi
+
+    elif [[ "$1" == "acl-composer" ]]; then
+        shift 1
+
+        if [[ "$EXEC" == "yes" ]]; then
+            ${COMPOSE} exec acl_php \
+                composer "$@"
+
+        else
+            ${COMPOSE} run --rm -w /var/www/html acl_php \
+                composer "$@"
+        fi
+
+
+    else
+        ${COMPOSE} "$@"
+    fi
+else
+    ${COMPOSE} ps
+fi
